5ae91f8c589e51d0d3c45a0c1a78252f101b924e
AnmolSrivastava376 <anmol.srivastva@accolitedigital.com>
2024-03-13 22:47:59 +0530

    Db

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MongoURL)
+        await mongoose.connect("mongodb+srv://anmolsrivastava:anmol123@mydatabase.ejnwb8l.mongodb.net/?retryWrites=true&w=majority&appName=MyDatabase/expenses")
         console.log(`Server running on ${mongoose.connection.host}`)
     } catch (err) {
         console.log(err)
